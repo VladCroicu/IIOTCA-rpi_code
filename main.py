7187f4aaab3df76a91c85b87c5fda58e0a33f918
VladCroicu <103421035+VladCroicu@users.noreply.github.com>
2023-05-23 14:41:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-#Modified by smartbuilds.io
-#Date: 27.09.20
-#Desc: This web application serves a motion JPEG stream
-# main.py
-# import the necessary packages
 from flask import Flask, render_template, Response, request, send_from_directory
 from camera import VideoCamera
 import os
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+pi_camera = VideoCamera(flip=False)
 
-# App Globals (do not edit)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') #you can customze index.html here
+    return render_template('index.html')
 
 def gen(camera):
-    #get camera frame
+
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
@@ -28,7 +22,6 @@ def video_feed():
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Take a photo when pressing camera button
 @app.route('/picture')
 def take_picture():
     pi_camera.take_picture()
@@ -36,4 +29,4 @@ def take_picture():
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='192.168.252.64', debug=False)
